799535713ac9772f10b55bdd9dfe4f8dea596662
codingang <angshu.gupta789@gmail.com>
2015-06-18 13:41:33 +0530

    dated=-6/18/15

---
+++ Object.js
@@ -0,0 +1,8 @@
+var person =  new Object();
+person["firstname"]="Angshuman";
+person["lastname"]="Gupta"
+
+var firstNameProperty = "firstname";
+
+console.log(person);
+console.log(person[firstNameProperty]);
