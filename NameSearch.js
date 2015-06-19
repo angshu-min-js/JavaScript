230830c92911585f1234e67e4a2f16fbf8611047
codingang <angshu.gupta789@gmail.com>
2015-06-19 19:59:43 +0530

    Update NameSearch.js

--- NameSearch.js
+++ NameSearch.js
@@ -1,25 +1,18 @@
 /*jshint multistr:true */
-
-text = "Blah blah blah blah blah blah Eric \
-blah blah blah Eric blah blah Eric blah blah \
-blah blah blah blah blah Eric";
-
-var myName = "Eric";
+var text = "Ang Ang";
+var myName = "Ang";
 var hits = [];
-
-// Look for "E" in the text
-for(var i = 0; i < text.length; i++) {
-	if (text[i] === "E") {
-		// If we find it, add characters up to
-		// the length of my name to the array
-		for(var j = i; j < (myName.length + i); j++) {
-			hits.push(text[j]);
-		}
-	}
+for(var i=0;i<text.length;i++)
+{
+    if(text[i]==="A")
+    {
+        for(var j=i;j<(myName.length+i);j++)
+        {
+            hits.push(text[j]);
+        }
+    }
 }
-
-if (hits.length === 0) {
-	console.log("Your name wasn't found!");
-} else {
-	console.log(hits);
-}
+if(hits.length===0)
+console.log("Your name wasn't found!");
+else
+console.log(hits);
