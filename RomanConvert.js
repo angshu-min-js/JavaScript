a4f12edee1e0277eaffe5a141fc4516febcc4998
Angshuman Gupta <angshu.gupta789@gmail.com>
2015-07-10 21:02:21 +0530

    Create RomanConvert.js

---
+++ RomanConvert.js
@@ -0,0 +1,14 @@
+function convert (num) {
+    var digits = String(num).split(""),
+        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+               "","I","II","III","IV","V","VI","VII","VIII","IX"],
+        roman = "",
+        i = 3;
+    while (i--)
+    {roman = (key[+digits.pop() + (i * 10)] || "") + roman;}
+  
+  return String(roman);
+}
+
+convert(36);
