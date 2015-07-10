9bf8fc107d031e7fd329be7eda8ffbf1b8fbe338
Angshuman Gupta <angshu.gupta789@gmail.com>
2015-07-10 20:32:41 +0530

    Create symmetric difference.js

---
+++ symmetric difference.js
@@ -0,0 +1,15 @@
+function diff(a1, a2) {
+  var result = [];
+  for (var i = 0; i < a1.length; i++) {
+    if (a2.indexOf(a1[i]) === -1) {
+      result.push(a1[i]);
+    }
+  }
+  for (i = 0; i < a2.length; i++) {
+    if (a1.indexOf(a2[i]) === -1) {
+      result.push(a2[i]);
+    }
+  }
+  return result;
+}
+diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
