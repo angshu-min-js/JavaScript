00f7535786704e89d5dfc1afdaf9ec661d9d8ef1
Angshuman Gupta <angshu.gupta789@gmail.com>
2015-07-11 12:31:46 +0530

    Create PigLatin.js
    Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
    If a word begins with a vowel you just add "way" to the end.

---
+++ PigLatin.js
@@ -0,0 +1,10 @@
+function translate(str) {
+ var index;
+ index = str.search(/[aeiouAEIOU]/);
+ if (index <= 0) { return str + "way"; }
+            else { return str.substring(index, str.length) + str.substring(0,index) + "ay"; }
+}
+translate("glove");
+<!--Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+
+If a word begins with a vowel you just add "way" to the end.-->
